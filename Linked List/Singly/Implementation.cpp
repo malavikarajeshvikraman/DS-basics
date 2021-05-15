9b206ac637e43fc3729c3724690fd7b391bba139
malavikarajeshvikraman <malu.rajesh2000@gmail.com>
2021-05-15 14:53:30 +0530

    Linked List

---
+++ Linked List/Singly/Implementation.cpp
@@ -0,0 +1,60 @@
+#include<iostream>
+using namespace std;
+
+
+struct node {
+  int data;
+  struct node *next;
+}*list,*head;
+
+void createlist(int n)
+{
+  list=(struct node *)malloc(sizeof(struct node));
+  struct node *tmp;
+  int num;
+  cout<<"\n Enter the first element in LL : ";
+  cin>>num;
+  list->data=num;
+  list->next=NULL;
+  head=list;
+  for(int i=1;i<n;i++)
+  {  tmp=(struct node *)malloc(sizeof(struct node));
+     if(tmp==NULL)
+     {
+       cout<<"No memory available";
+       break;
+     }
+     else
+     {
+       cout<<"\n Enter the element :";
+       cin>>num;
+       list->next=tmp;
+       tmp->data=num;
+       tmp->next=NULL;
+       list=tmp;
+     }
+
+  }
+}
+void display()
+{ struct node *tmp;
+  tmp=head;
+  while(tmp->next!=NULL)
+  {
+    cout<<"\n"<<tmp->data;
+    tmp=tmp->next;
+  }  
+  cout<<"\n"<<tmp->data;
+}
+
+
+int main(){
+int n;
+cout<<"Enter the number of nodes ";
+cin>>n;
+createlist(n);
+display();
+
+
+return 0;
+}
